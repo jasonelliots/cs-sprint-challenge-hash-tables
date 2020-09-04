2f1c266a1b756e5b89c75a1494f2103941b3ab05
jasonelliots <j.elliotschwartz@gmail.com>
2020-09-04 10:26:27 -0700

    almost there!

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,17 +1,48 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
+  
     # Your code here
+    d = {}
+
+    for index, l in enumerate(arrays):
+        for num in l:
+            if num not in d:
+                # set a key for that number with it's value as an array containing the index of list it appeared in 
+                d[num] = [index]
+            else:
+                # add the current list index to the entry for this num 
+                d[num].append(index)
+    
+
+    # if len(arrays) = len(the value of that key), we have an intersection point, put that key in the result 
+
+    # may have to convert array values to tuples to eliminate dups 
+
+    result = []
+
+    for key in d:
+        # might need to change to tuple here 
+    
+        if len(d[key]) == len(arrays):
+            result.append(key)
+
 
     return result
 
+array1 = [
+    [1, 2, 3, 4, 5],
+    [12, 7, 2, 9, 1],
+    [99, 2, 7, 1,]
+]
 
-if __name__ == "__main__":
-    arrays = []
+print(intersection(array1))
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
-    print(intersection(arrays))
+
+# if __name__ == "__main__":
+#     arrays = []
+
+#     arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+#     arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+#     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+
+#     print(intersection(arrays))
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -4,8 +4,29 @@ def has_negatives(a):
     """
     # Your code here
 
+    d = {}
+
+    for n in a:
+        if n in d:
+            continue
+        else:
+            d[n] = 1
+
+    result = []
+
+    for n in d:
+        # if abs(n) in result:
+        #     continue 
+        if -n in d:
+            if abs(n) not in result:
+                result.append(abs(n))
+
     return result
 
+
+# array1 = [ 1, -1, 2, 3, -4, -3, 4, -5, 6, 7 ]
+
+# print(has_negatives(array1))
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
